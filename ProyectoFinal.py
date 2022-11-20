feb0c673d210982c43b69c64225d113a8cbb9250
Gabriel044 <gabgar4@gmail.com>
2022-11-19 23:58:17 -0600

    Update to latest class example

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -1,195 +1,243 @@
-# -----------------------------------------------------------------------------
-# calc.py
-#
-# A simple calculator with variables.   This is from O'Reilly's
-# "Lex and Yacc", p. 63.
-# -----------------------------------------------------------------------------
-
 import sys
-sys.path.insert(0, "../..")
-
-tokens = (
-    'NAME', 'INUMBER', 'FNUMBER', 'BOOLEAN'
-)
-
-literals = ['=', '+', '-', 'i', 'f', 'b', 'p', '{', '}', '(', ')']
-reserved =  {
-    'if' : 'IF',
-    'then': 'THEN',
-    'else': 'ELSE'
+sys.path.insert(0, "..")
+reserved = {
+    "int": "INTDCL",
+    "float": "FLOATDCL",
+    "print": "PRINT",
+    "boolean": "BOOLDCL",
+    "true": "BOOLVAL",
+    "false": "BOOLVAL",
+    "if": "IF",
+    "else": "ELSE"
 }
+tokens = [
+    'NAME', 'INUMBER', 'FNUMBER',
+]
+tokens.extend(reserved.values())
+literals = ['=', '+', '-', ';', '(', ')', '{', '}']
 # Tokens
-
-t_NAME = r'[ac-eg-hj-oq-z]'
-
-
-
+def t_NAME(t):
+    r'[a-zA-Z_]+[a-zA-Z0-9]*' #r'[a-eg-hj-oq-z]'
+    if t.value in reserved:
+        t.type = reserved[t.value]
+    return t
 def t_FNUMBER(t):
     r'\d+\.\d+'
     t.value = float(t.value)
     return t
-
-
 def t_INUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
-
-def t_BOOLEAN(t):
-    r'True|False'
-    t.value = bool(t.value)  
-
-def t_lbrace(t):
-     r'\{'
-     t.type = '{'      
-     return t
- 
- def t_rbrace(t):
-     r'\}'
-     t.type = '}'
-     return t
-
-
 t_ignore = " \t"
-
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
-
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-
 # Build the lexer
 import ply.lex as lex
 lexer = lex.lex()
-
-# Test
-data = '''
-10.4 
-11
-False
- '''
-lexer.input(data)
-
-while True:
-    tok = lexer.token()
-    print(tok)
-    if not tok:
-        break;
-    print(tok)
-    print(tok.type, tok.value, tok.lineno, tok.lexpos) # lexpos es posición en tabla de símbolos.
-
 # Parsing rules
 class Node:
-    def __init__(self,type,children=None,leaf=None):
-        self.type = type
-        if children:
-            self.children = children
-        else:
-            self.children = [ ]
-        self.leaf = leaf
+    
+    # childrens = None
+    # type = None
+    def __init__(self):
+        self.childrens = []
+        self.type = ''
+        self.val = ''
+    def print(self, lvl = 0):
+        r = (' ' * lvl) + self.type + ":" + str(self.val)
+        print(r)
+        #print(self.childrens)
+        for c in self.childrens:
+            c.print(lvl+1)
+        
 # dictionary of names
-names = {}
-abstractTree = {}
-
+symbolsTable = {
+    "table" : {},
+    "parent" : None,
+}
+abstractTree = None
 def p_prog(p):
-    'prog : dcls stmts'
-    None
-
-def p_dcls(p):
-    '''dcls : dcl dcls
-            | dcl'''
-    None
-
-def p_dcl_declare_int(p):
-    'dcl : "i" NAME'
-    names[p[2]] = { "type": "INT", "value":0}
-
-def p_statement_declare_float(p):
-    'dcl : "f" NAME'
-    names[p[2]] = { "type": "FLOAT", "value":0}
-
-
+    'prog : stmts'
+    global abstractTree
+    abstractTree = Node()
+    abstractTree.type = 'root'
+    abstractTree.childrens.extend(p[1])
 def p_statements_recursion(p):
     '''stmts : statement stmts
-             | statement'''
-    None
+             | statement '''
+    stmt = p[1]
+    if len(p) == 3:
+        stmts = [ stmt ]
+        stmts.extend(p[2])
+        p[0] = stmts
+    else: 
+        p[0] = [ stmt ]
+    
+def p_dcl_declare_int(p):
+    'statement : INTDCL NAME ";"'
+    symbolsTable["table"][p[2]] = { "type": "INT", "value":0}
+    n = Node()
+    n.type = "INT_DLC"
+    n.val = p[2]
+    p[0] = n
+def p_statement_declare_float(p):
+    'statement : FLOATDCL NAME ";"'
+    symbolsTable["table"][p[2]] = { "type": "FLOAT", "value":0 }
+    n = Node()
+    n.type = "FLOAT_DLC"
+    n.val = p[2]
+    p[0] = n
 
+def p_statement_declare_bool(p):
+    'statement : BOOLDCL NAME ";"'
+    symbolsTable["table"][p[2]] = { "type": "BOOLEAN", "value": False }
+    n = Node()
+    n.type = "BOOL_DLC"
+    n.val = p[2]
+    p[0] = n
 def p_statement_print(p):
-    'statement : "p" expression'
-    print(p[2])
-
+    'statement : PRINT expression ";"'
+    n = Node()
+    n.type = 'PRINT'
+    n.childrens.append(p[2])
+    p[0] = n
+def p_statement_if(p):
+    'statement : IF "(" boolexp ")" "{" stmts "}"'
+    n = Node()
+    n.type = 'IF'
+    n2 = Node()
+    n2.childrens = p[6]
+    n.childrens.append(p[3])
+    n.childrens.append(n2)
+    p[0] = n
 def p_statement_assign(p):
-    'statement : NAME "=" expression'
-    if p[1] not in names:
+    'statement : NAME "=" expression ";"'
+    if p[1] not in symbolsTable["table"]:
         print ( "You must declare a variable before using it")
-    if isinstance(p[3], float) and names[p[1]]["type"] != "FLOAT":
-        print ( "You should not assign a float value to a int variable")
-    names[p[1]]["value"] = p[3]
-
-
-# def p_statement_expr(p):
-#     'statement : expression'
-#     # print(p[1])
-
-
+    n = Node()
+    n.type = 'ASIGN'
+    ##n.childrens.append(p[1])
+    if p[1] in symbolsTable["table"]:
+        n1 = Node()
+        n1.type = 'ID'
+        n1.val = p[1]
+        n.childrens.append(n1)
+    else: 
+        print("Error undeclared variable")
+    n.childrens.append(p[3])
+    p[0] = n
+def p_expression_group(p):
+    "expression : '(' expression ')'"
+    p[0] = p[2]
 def p_expression_binop(p):
     '''expression : expression '+' expression
                   | expression '-' expression'''
     if p[2] == '+':
-        p[0] = p[1] + p[3]
+        n = Node()
+        n.type = '+'
+        n.childrens.append(p[1])
+        n.childrens.append(p[3])
+        p[0] = n
     elif p[2] == '-':
-        p[0] = p[1] - p[3]
-
-
-def p_expression_group(p):
-    "expression : '(' expression ')'"
-    p[0] = p[2]
-
+        n = Node()
+        n.type = '-'
+        n.childrens.append(p[1])
+        n.childrens.append(p[3])
+        p[0] = n
 
 def p_expression_inumber(p):
     "expression : INUMBER"
-    p[0] = p[1]
-
-
+    n = Node()
+    n.type = 'INUMBER'
+    n.val = int(p[1])
+    p[0] = n
 def p_expression_fnumber(p):
     "expression : FNUMBER"
+    n = Node()
+    n.type = 'FNUMBER'
+    n.val = float(p[1])
+    p[0] = n
+def p_expression_boolval(p):
+    "expression : boolexp"
     p[0] = p[1]
-
-def p_expression_fnumber(p):
-    "expression : BOOLEAN"
-    p[0] = p[1]
-
-
+def p_bool_expression(p):
+    "boolexp : BOOLVAL"
+    n = Node()
+    n.type = 'BOOLVAL'
+    n.val = (p[1] == 'true')
+    p[0] = n
 def p_expression_name(p):
     "expression : NAME"
-    try:
-        p[0] = names[p[1]]["value"]
-    except LookupError:
-        print("Undefined name '%s'" % p[1])
-        p[0] = 0
-
-
+    if p[1] in symbolsTable["table"]:
+        n = Node()
+        n.type = 'ID'
+        n.val = p[1]
+        p[0] = n
 def p_error(p):
     if p:
         print("Syntax error at '%s'" % p.value)
     else:
         print("Syntax error at EOF")
-
 import ply.yacc as yacc
 parser = yacc.yacc()
 
-""" 
 f = open("code.txt")
 content = f.read()
-yacc.parse(content) """
-
-# while True:
-#     try:
-#         s = input('calc > ')
-#     except EOFError:
-#         break
-#     if not s:
-#         continue
-#     yacc.parse(s)
+yacc.parse(content)
+abstractTree.print()
+varCounter = 0
+labelCounter = 0
+def genTAC(node):
+    global varCounter
+    global labelCounter
+    if ( node.type == "ASIGN" ):
+        print(node.childrens[0].val  + " := " + genTAC(node.childrens[1]) )
+    elif ( node.type == "INUMBER"):
+        return str(node.val)
+    elif ( node.type == "+"):
+        tempVar = "t" + str(varCounter)
+        varCounter = varCounter +1
+        print( tempVar + " := " + genTAC(node.childrens[0]) + " + " + 
+genTAC(node.childrens[1]))
+        return tempVar
+    elif ( node.type == "PRINT"):
+        print( "PRINT " + genTAC(node.childrens[0]))
+    elif ( node.type == "IF" ):
+        tempVar = "t" + str(varCounter)
+        varCounter = varCounter +1
+        print ( tempVar + " := !" + str(node.childrens[0].val))
+        tempLabel = "l" + str(labelCounter)
+        labelCounter = labelCounter + 1
+        print ( "gotoLabelIf " + tempVar + " " + tempLabel)
+        genTAC(node.childrens[1])
+        print ( tempLabel)
+    else:
+        for child in node.childrens:
+            genTAC(child)
+    
+print ("\ntac:\n")
+genTAC(abstractTree)
+#Some examples
+# for ( i = 0; i < 3; i++){
+#     stamentes
+# }
+# i := 0
+# t1 = i < 3
+# t0 = !t1
+# gotoLabelif t0 Label1
+# staments
+# i = i + 1
+# Label1
+# while ( condicion ) {
+#     staments
+# }
+# WHILE
+# t1 = condicion
+# t0 = !t1
+# gotoLabelif t0 Label1
+# staments
